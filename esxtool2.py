3f8794d0c28a891ec151271d5eed468e9406cd41
Michał Kowalik (kowalikm) <kowalikm@cisco.com>
2024-12-17 14:19:39 +0100

    updated licensing

--- esxtool2.py
+++ esxtool2.py
@@ -9,9 +9,23 @@ Example use cases:
 
 Tested on Ekahau AI Pro 11.4 / 11.5 / 11.6
 
-This software is licensed under the Cisco Sample Code License
-
 https://github.com/ciscowireless/esxtool2
+
+
+Copyright (c) 2024 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
 """
 import os
 import argparse
